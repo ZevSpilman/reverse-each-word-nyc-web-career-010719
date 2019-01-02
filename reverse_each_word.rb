698b1af9b438c7ed7ab3ed4212e2565c8d6aab9e
Zev Spilman <zevspilman@yahoo.com>
2019-01-02 22:49:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,9 @@ def reverse_each_word(string)
   newArr = string.split(" ")
  newArr.each do |thing|
    newNewArr.push(thing.reverse)
-   return newNewArr.join(" ")
+   
 end
+return newNewArr.join(" ")
 end
 
 reverse_each_word(string)
